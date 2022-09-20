bbb4df74c7cb26ab16b14a3f057ba5497be3b2b6
Shyan Roy Choudhury <83347717+ShyanRoyChoudhury@users.noreply.github.com>
2022-09-20 17:27:41 +0530

    Update Reverse Nodes in k-Group.py

--- Linked List/Reverse Nodes in k-Group.py
+++ Linked List/Reverse Nodes in k-Group.py
@@ -74,3 +74,70 @@ Time  Complexity: O(N)
 Space Complexity: O(N / K) { Recursive Stack Space }
 
 """
+""" APPROACH 2: USING ITERATION
+we will NOT be using any extra Recursive Stack Space
+To save space, we will use be using iterative approach to Reverse Nodes in Groups of K
+Instead of using Start, here we will be using beforeStart which points to the Node before Start Pointer
+We also need to store the End's Next in temp variable
+After reversing, we just have to make:
+a) beforeStart -> next = End
+b) Start -> next = temp
+Move beforeStart to Start & end to temp for next set of reversals
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        def reverse(start, end):
+            prev = None
+            cur = start
+            nextNode = start.next
+            
+            while prev != end:
+                cur.next = prev
+                prev = cur
+                cur = nextNode
+                
+                if nextNode is not None:
+                    nextNode = nextNode.next
+                    
+        if k ==1 or not head or not head.next:
+            return head
+        
+        dummy = ListNode(-1) #creating a dummy to assign its next to head
+        dummy.next = head
+        beforeStart = dummy 
+        
+        end = head
+        i = 0   # iterator to keep track of no. of nodes traversed
+        
+        while end is not None:
+            i += 1
+            if i%k == 0:
+                start = beforeStart.next    # start of the node
+                temp = end.next # temp stores the end.next since we lose the connection during reversal
+            
+                reverse(start, end) #function to reverse the k group
+                
+                # restoring the connections of reverse group to the ll
+                beforeStart.next = end #  After reversing is done, beforeStart's next should now point to End pointer
+                start.next = temp   #  Start's next should point to temp(intial End's Next)
+                
+                # After tat, beforeStart should be moved to the place of Start Pointer
+
+                beforeStart = start
+                end = temp
+            else:
+                # Else if we haven't covered the LinkedList of Length K, we make End move to it's Next Node
+                end = end.next
+        # At the end, we return Dummy's Next which will be our new head after reversing all the nodes in k group        
+        return dummy.next
