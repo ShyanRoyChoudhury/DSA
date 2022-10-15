e0cc9992cdf1166f5e78e9a607ec7fe7471852c6
Shyan Roy Choudhury <83347717+ShyanRoyChoudhury@users.noreply.github.com>
2022-10-15 21:23:25 +0530

    Create  Implement Queue using Stacks.py

---
+++ Stack Queue/Implement Queue using Stacks.py
@@ -0,0 +1,36 @@
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        ans = self.s2.pop()
+
+        while self.s2:
+            self.s1.append(self.s2.pop())
+        return ans
+
+    def peek(self) -> int:
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        
+        ans = self.s2[-1]
+
+        while self.s2:
+            self.s1.append(self.s2.pop())
+        
+        return ans
+        
+    def empty(self) -> bool:
+        return len(self.s1) == 0
+      
+"""
+T.C:O(N)
+S.C:
+"""
