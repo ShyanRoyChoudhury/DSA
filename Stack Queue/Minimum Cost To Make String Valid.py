9688d0b6ce7b90824aabb1c298ab1254e2754b81
Shyan Roy Choudhury <83347717+ShyanRoyChoudhury@users.noreply.github.com>
2022-10-28 15:23:39 +0530

    Update Minimum Cost To Make String Valid.py

--- Stack Queue/Minimum Cost To Make String Valid.py
+++ Stack Queue/Minimum Cost To Make String Valid.py
@@ -4,19 +4,19 @@
 def findMinimumCost(str):
     stack = []
     #odd condition
-    if len(str) % 2 != 0:
+    if len(str) % 2 != 0:       # if length of string is odd, it is impossible to make it valid
         return -1
     
     for ch in str:
         if ch == '{':
             stack.append(ch)
         else:
-            if stack and stack[-1] == '{':
+            if stack and stack[-1] == '{':      # if the '{}' condition checks out we skip the correct bracket and continue to the next 'invalid bracket'
                 stack.pop()
             else:
                 stack.append(ch)
     a, b = 0, 0
-    while stack:
+    while stack:        # a and b represents no of open brackets '{' and closed brackets '}' respectively.
         if stack[-1] == '{':
             a += 1
         else:
