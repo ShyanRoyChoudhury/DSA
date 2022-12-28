c4b250e963731cb2b8e5d21199e344f209c8fe20
Shyan Roy Choudhury <83347717+ShyanRoyChoudhury@users.noreply.github.com>
2022-12-28 19:39:32 +0530

    Create Binary Search.py

---
+++ Binary Search/Binary Search.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l<=r:
+            m = (l+r)//2
+            if target<nums[m]:
+                r = m-1
+            elif target>nums[m]:
+                l = m+1
+            else:
+                return m
+        return -1
+      
+      
+"""
+T.C:O(log n)
+S.C:O()
+"""
